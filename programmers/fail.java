09a6853bd008f305f950d32887421c0639a50d62
hyojin530 <dkakfl56@gmail.com>
2021-09-18 22:54:46 +0900

    9/18

--- programmers/fail.java
+++ programmers/fail.java
@@ -1,41 +1,61 @@
-//https://programmers.co.kr/learn/courses/30/lessons/42889
+//https://programmers.co.kr/learn/courses/30/lessons/42889 (re)
 
 import java.util.*;
 
 class Solution {
-    public int[] solution(int N, int[] stages) {
-        int[] answer = new int[N];
-        HashMap<Integer, Integer> stageMap = new HashMap<>();
-        HashMap<Integer, Integer> reachMap = new HashMap<>();
-        HashMap<Integer, Float> failMap = new HashMap<>();
+    
+    static class Stage implements Comparable<Stage>{
+        int stageNum;
+        double fail;
         
-        for(int stage: stages){
-            stageMap.put(stage, stageMap.getOrDefault(stage, 0)+1);
+        public Stage(int stageNum, double fail){
+            this.stageNum = stageNum;
+            this.fail = fail;
         }
         
-        int reach = 0;
-        for(int i=N; i>0; i--){
-            reach += stageMap.getOrDefault(i, 0);
-            reachMap.put(i, reach);
-        }
-        
-        for(int i=1; i<=N; i++){
-            int clear = stageMap.getOrDefault(N+1, 0).intValue();
-            int stageNum = stageMap.getOrDefault(i, 0).intValue();
-            int reachNum = reachMap.getOrDefault(i, 0).intValue() + clear;
-            float fail = 0;
-            if(stageNum==0 || reachNum==0){
-                fail = 0;
-                failMap.put(i, (float)0);
-            } else{
-                fail = (float)stageNum/(float)reachNum;
-                failMap.put(i, fail);
+        @Override
+        public int compareTo(Stage s){
+            if(this.fail == s.fail){
+                return this.stageNum - s.stageNum;
+            } else if(this.fail > s.fail){
+                return -1;
+            } else {
+                return 1;
             }
         }
         
-        List<Map.Entry<Integer, Float>> entryList = new LinkedList<>(failMap.entrySet());
-        Collections.sort(entryList, (f1, f2) -> f1.getValue().compareTo(f2.getValue()));
+    }
+    
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
         
+        List<Stage> stageList = new ArrayList<Stage>();
+        Arrays.sort(stages);
+        int size = stages.length;
+        int index = 0;
+
+        for(int i=1; i<=N; i++){
+            int cnt = 0;
+            for(int j=index; j<stages.length; j++){
+                if(stages[j]>i){
+                    break;
+                }
+                cnt++;
+                index++;
+            }
+            if(cnt==0){
+                stageList.add(new Stage(i, 0));
+            } else{
+                stageList.add(new Stage(i, (double)cnt/size));
+                size -= cnt;
+            }
+        }
+        
+        Collections.sort(stageList);
+        for(int i=0; i<N; i++){
+            answer[i] = stageList.get(i).stageNum;
+        }
+
         return answer;
     }
 }
